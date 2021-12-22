16bc13f6899e7477a5fae04f994f58966c42d230
linfengzhong <fred.zhong@outlook.com>
2021-12-22 14:40:15 +0800

    fx

--- smart-tool-v5.sh
+++ smart-tool-v5.sh
@@ -204,11 +204,11 @@ function install_prerequisite () {
 		print_error "sysstat usbutils pciutils lshw iperf iperf3 neofetch psmisc tree python3-dev python3-pip bpytop nmap p7zip-full rfkill hwinfo hdparm htop 已经安装，无需重复操作！"
 	else
 		print_info "安装进行中ing "	
-		$upgrade
+		$upgrade >/dev/null 2>&1
 		print_info "wget lsof tar unzip curl socat bind9-utils jq"
-		$installType wget lsof tar unzip curl socat bind9-utils jq
+		$installType wget lsof tar unzip curl socat bind9-utils jq >/dev/null 2>&1
 		print_info "sysstat usbutils pciutils lshw iperf iperf3 neofetch psmisc tree python3-dev python3-pip bpytop nmap p7zip-full rfkill hwinfo hdparm htop"
-		$installType sysstat usbutils pciutils lshw iperf iperf3 neofetch psmisc tree python3-dev python3-pip bpytop nmap p7zip-full rfkill hwinfo hdparm htop
+		$installType sysstat usbutils pciutils lshw iperf iperf3 neofetch psmisc tree python3-dev python3-pip bpytop nmap p7zip-full rfkill hwinfo hdparm htop >/dev/null 2>&1
 		mkdir -p /etc/fuckGFW/prerequisite
 		touch /etc/fuckGFW/prerequisite/check
 	fi
